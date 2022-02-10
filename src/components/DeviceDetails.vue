40356bc74c789e5b9c7d03986e708edc898d38ff
Artur Frącek <arturfracek7@gmail.com>
2022-02-10 20:09:21 +0100

    dragging added

--- src/components/DeviceDetails.vue
+++ src/components/DeviceDetails.vue
@@ -1,17 +1,44 @@
 <template>
-  <div class="deviceDetails">
-    123
-  </div>
+  <div class="deviceDetails__draggable">{{ widget }}</div>
 </template>
 
 <script>
-export default {
+import interact from "interactjs";
+let position = { x: 0, y: 0 };
 
-}
+interact(".deviceDetails__draggable").draggable({
+  listeners: {
+    start(event) {
+      console.log(event.type, event.target);
+    },
+    move(event) {
+      position.x += event.dx;
+      position.y += event.dy;
+
+      event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+    },
+  },
+});
+export default {
+  props: {
+    widget: {
+      type: Object,
+      required: false,
+    },
+  },
+};
 </script>
 
 <style>
-.deviceDetails {
-  height:100vh
+.deviceDetails__draggable {
+  position: absolute;
+  width: 25%;
+  min-height: 6.5em;
+  background-color: #29e;
+  color: white;
+  border-radius: 0.75em;
+  padding: 4%;
+  touch-action: none;
+  user-select: none;
 }
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -6,7 +6,7 @@
         :key="index"
         :device="device"
       />
-      <DeviceDetails v-if="$route.params.deviceId" />
+      <DeviceDetails :widget="widget" v-if="$route.params.deviceId" />
     </div>
   </div>
 </template>
@@ -34,9 +34,9 @@ export default {
       immediate: true, // true jezeli ma sie odpalic odrazu na mouted
       async handler(deviceId) {
         // handler to funcka która odpali się kiedy obserwowana wartosc sie zmieni ( wszystko z this., z $route, z vuexa) a lineId to Nowa wartosc po zmianie
-       if(!deviceId) return
-       this.widget = await fetchDeviceById(deviceId); // ciało funkcji
-        console.log(deviceId)
+        if (!deviceId) return;
+        this.widget = await fetchDeviceById(deviceId); // ciało funkcji
+        console.log(deviceId);
         console.log(this.widget);
       },
     },
@@ -44,7 +44,6 @@ export default {
   async beforeMount() {
     const res = await fetchAllDevices();
     this.devices = res;
-    
   },
 };
 </script>
